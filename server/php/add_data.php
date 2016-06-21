9c57151f3edca1244ba044712267803ea4574a2d
Tom Willis <tomwillis608@gmail.com>
2016-06-21 16:06:08 -0500

    Add pressure measurement, improve syntax of INSERT statement.

--- server/php/add_data.php
+++ server/php/add_data.php
@@ -8,8 +8,13 @@ include("dbconnect.php");
 $sensor = $_GET["serial"];
 $celsius = $_GET["temperature"];
 $humidity = $_GET["humidity"];
-	
-$SQL = "INSERT INTO `dht22` (sensor,celsius,humidity) VALUES ($sensor,$celsius,$humidity)";
+$pressure = $_GET["pressure"];
+echo "Sensor: " . $sensor . PHP_EOL;
+echo "Temperature: " . $celsius . PHP_EOL;
+echo "Humidity: " . $humidity . PHP_EOL;
+echo "Pressure: " . $pressure . PHP_EOL;	
+$SQL = "INSERT INTO `dht22` (sensor,celsius,humidity,pressure) VALUES ('$sensor','$celsius','$humidity','$pressure')";
+
 // Execute SQL statement
 mysqli_query($link, $SQL);
 ?>
